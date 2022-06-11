6cf768c0889edeb6af3b0fee68d731a3b6073236
anonymousip1234 <89058595+anonymousip1234@users.noreply.github.com>
2022-06-11 18:51:19 +0530

    Add files via upload

---
+++ PartTimeFullTime.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    internal class PartTimeFullTime
+    {
+        public int Present = 1;
+        public int wage_hour = 20;
+        public int FullTImeHour = 8;
+        public int PartTImeHour = 4;
+        public int FullTime = 1;
+
+
+        public void DAILYWAGE()
+        {
+            Random value = new Random();
+            int binary = value.Next(0, 2);
+
+            if (binary == Present)
+            {
+                Random check = new Random();
+                int PTorFT = check.Next(0, 2);
+
+                if (PTorFT == FullTime)
+                    {
+                    int wage = wage_hour * FullTImeHour;
+
+                    Console.WriteLine("Employee is Present" + " Wage is " + wage);
+                }
+                else
+                {
+                    int wage = wage_hour * PartTImeHour;
+                    Console.WriteLine("Employee is Present" + " Wage is " + wage);
+                }
+               
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
