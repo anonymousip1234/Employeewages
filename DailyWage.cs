760f85b35157eb944e8b51f6c2c5bda194f05148
anonymousip1234 <89058595+anonymousip1234@users.noreply.github.com>
2022-06-11 18:50:19 +0530

    Add files via upload

---
+++ DailyWage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    internal class DailyWage
+    {
+        public int Present = 1;
+        public int wage_hour = 20;
+        public int total_hour = 8;
+
+
+        public void DAILYWAGE()
+        {
+            Random value = new Random();
+            int binary = value.Next(0, 2);
+
+            if (binary == Present)
+            {
+                int wage = wage_hour * total_hour;
+                
+                Console.WriteLine("Employee is Present" +  " Wage is " + wage);
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+
+}
